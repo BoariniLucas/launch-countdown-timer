16b469ce01d2c494a827af45cfd1e6f36ef21e26
Lucas Boarini <lcsboarinireis92@gmail.com>
2023-04-25 11:07:31 -0300

    :sparkles: Implementado tela e funcionalidade de escolha de data

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,10 +8,16 @@ const hoursBoxFlip = document.querySelector('#box-hours');
 const minutesBoxFlip = document.querySelector('#box-minutes');
 const secondsBoxFlip = document.querySelector('#box-seconds');
 const timeBoxCountdown = document.querySelector('.time-box-countdown');
+const dateTime = document.querySelector('#date-time');
+const launchDateForm = document.querySelector('.launch-date');
 
-//Selected date
+const btnOpenDateForm = document.querySelector('#btn-open-date-form');
+const btnClose = document.querySelector('#btn-close');
+const btnClearForm = document.querySelector('#btn-clear-form');
+const btnSelectDate = document.querySelector('#btn-select-date');
+
 let countDownDate = new Date("Jun 29, 2023 20:35:00").getTime();
-
+let selectData = 0;
 let days = '';
 let hours = '';
 let minutes = '';
@@ -22,61 +28,102 @@ hoursBoxFlip.classList.toggle("ok");
 minutesBoxFlip.classList.toggle("ok");
 secondsBoxFlip.classList.toggle("ok");
 
-//Function 
-let interval = setInterval(function() {
+countDownFunction();
 
-  let now = new Date().getTime();
 
-  let distance = countDownDate - now;
+/*     ----Events----     */
+/*        --------        */
+btnOpenDateForm.addEventListener('click', () => {
+  launchDateForm.style.visibility = "visible";
+});
 
-  let daysCont = Math.floor(distance / (1000 * 60 * 60 * 24));
-  let hoursCont = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  let minutesCont = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  let secondsCont = Math.floor((distance % (1000 * 60)) / 1000);
+btnClose.addEventListener('click', () => {
+  launchDateForm.style.visibility = "hidden";
+});
 
-  if(days != daysCont) {
-    daysDisplay.innerHTML = daysCont;
-    days = daysCont;
+btnClearForm.addEventListener('click', () => {
+  clearForm();
+});
 
-    daysBoxFlip.classList.toggle("flip");
-    hoursBoxFlip.classList.toggle("ok");
-    minutesBoxFlip.classList.toggle("ok");
-    secondsBoxFlip.classList.toggle("ok");
-  } 
+btnSelectDate.addEventListener('click', () => {
 
-  if(hours != hoursCont) {
-    hoursDisplay.innerHTML = hoursCont;
-    hours = hoursCont;
+  selectData = 0;
+  countDownDate = 0;
 
-    daysBoxFlip.classList.toggle("ok");
-    hoursBoxFlip.classList.toggle("flip");
-    minutesBoxFlip.classList.toggle("ok");
-    secondsBoxFlip.classList.toggle("ok");
-  } 
+  selectData = dateTime.value; 
+  countDownDate = new Date(selectData).getTime();
+  console.log(countDownDate);
+
+  launchDateForm.style.visibility = "hidden";
   
-  if(minutes != minutesCont) {
-    minutesDisplay.innerHTML = minutesCont;
-    minutes = minutesCont;
+  countDownFunction();
+});
 
-    daysBoxFlip.classList.toggle("ok");
-    hoursBoxFlip.classList.toggle("ok");
-    minutesBoxFlip.classList.toggle("flip");
-    secondsBoxFlip.classList.toggle("ok");
-  } 
+
+
+/*     ----Functions----     */
+/*         ---------         */
+
+function clearForm() {
+  dateTime.value = "";
+}
+
+function countDownFunction() {
+  let interval = setInterval(function() {
+
+    let now = new Date().getTime();
   
-  if(seconds != secondsCont) {
-    secondsDisplay.innerHTML = secondsCont;
-    seconds = secondsCont;
+    let distance = countDownDate - now;
+  
+    let daysCont = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hoursCont = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutesCont = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let secondsCont = Math.floor((distance % (1000 * 60)) / 1000);
+  
+    if(days != daysCont) {
+      daysDisplay.innerHTML = daysCont;
+      days = daysCont;
+  
+      daysBoxFlip.classList.toggle("flip");
+      hoursBoxFlip.classList.toggle("ok");
+      minutesBoxFlip.classList.toggle("ok");
+      secondsBoxFlip.classList.toggle("ok");
+    } 
+  
+    if(hours != hoursCont) {
+      hoursDisplay.innerHTML = hoursCont;
+      hours = hoursCont;
+  
+      daysBoxFlip.classList.toggle("ok");
+      hoursBoxFlip.classList.toggle("flip");
+      minutesBoxFlip.classList.toggle("ok");
+      secondsBoxFlip.classList.toggle("ok");
+    } 
     
-    daysBoxFlip.classList.toggle("ok");
-    hoursBoxFlip.classList.toggle("ok");
-    minutesBoxFlip.classList.toggle("ok");
-    secondsBoxFlip.classList.toggle("flip");
-  }
-
-  if (distance < 0) {
-    clearInterval(interval);
-
-    alert("TIME EXPIRED");
-  }
-}, 1000);
+    if(minutes != minutesCont) {
+      minutesDisplay.innerHTML = minutesCont;
+      minutes = minutesCont;
+  
+      daysBoxFlip.classList.toggle("ok");
+      hoursBoxFlip.classList.toggle("ok");
+      minutesBoxFlip.classList.toggle("flip");
+      secondsBoxFlip.classList.toggle("ok");
+    } 
+    
+    if(seconds != secondsCont) {
+      secondsDisplay.innerHTML = secondsCont;
+      seconds = secondsCont;
+      
+      daysBoxFlip.classList.toggle("ok");
+      hoursBoxFlip.classList.toggle("ok");
+      minutesBoxFlip.classList.toggle("ok");
+      secondsBoxFlip.classList.toggle("flip");
+    }
+  
+    if (distance < 0) {
+      clearInterval(interval);
+  
+      alert("TIME EXPIRED");
+    }
+  }, 1000);
+}
